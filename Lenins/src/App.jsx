f138c9197718f04bf418a1fac48da1d4fdd2c173
rey <njongremy30@gmail.com>
2025-09-24 02:21:50 +0100

    minor header touches

---
+++ Lenins/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './header/header'
+
+ const App = () => {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
