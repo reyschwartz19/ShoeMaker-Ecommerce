b113cb903516ae22fead2553cd5aa54a0137ba78
rey <njongremy30@gmail.com>
2025-09-27 20:52:24 +0100

    .

--- Lenins/src/discover/discover.jsx
+++ Lenins/src/discover/discover.jsx
@@ -1,4 +1,9 @@
 import styles from './discover.module.css'
+import ProductCard from '../productCard/productCard';
+import loafer from '../assets/loafer1.jpg'
+import boot from '../assets/boot1.jpg'
+import oxford from '../assets/oxford1.jpg'
+import oxford2 from '../assets/oxford2.jpg'
 
 const Discover = () => {
     
@@ -9,6 +14,10 @@ const Discover = () => {
             <h2 className={`text-2xl font-bold my-4 ${styles.heading}`}>Discover</h2>
             <a href="#" className='underline text-gray-900'>More</a>
         </div>
+     
+        <div>
+           <ProductCard image={loafer} /> 
+        </div>   
         
        </section> 
     );
---
+++ Lenins/src/productCard/productCard.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+const ProductCard = (image) => {
+
+
+    return(
+        <div>
+            <img src={image}/>
+            <p>Product Name</p>
+            <p>$50.00</p>
+        </div>
+    );
+
+}
+
+export default ProductCard;
