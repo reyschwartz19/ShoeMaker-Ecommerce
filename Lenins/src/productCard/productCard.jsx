d31242ef98681e37c86496de42e658d61fec616c
rey <njongremy30@gmail.com>
2025-09-29 22:43:23 +0100

    .

--- Lenins/src/productCard/productCard.jsx
+++ Lenins/src/productCard/productCard.jsx
@@ -2,14 +2,18 @@
 
 
 
-const ProductCard = (image) => {
+const ProductCard = ({image}) => {
 
 
     return(
         <div>
-            <img src={image}/>
-            <p>Product Name</p>
-            <p>$50.00</p>
+            <div className={` w-[200px] h-[200px] bg-cover`}  style={{backgroundImage: `url(${image})`}}>
+            
+            </div>
+            <div>
+                <p>Product Name</p>
+                <p>$50.00 <span>$65.00</span></p>
+            </div>
         </div>
     );
 
